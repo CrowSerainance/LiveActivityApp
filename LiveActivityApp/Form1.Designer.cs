e2a79b65d618740346eebc8480633a518faf3dc9
chris <chris@CHRIS>
2025-08-31 08:34:32 +0800

    Add project files.

---
+++ LiveActivityApp/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace LiveActivityApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            btnStart = new Button();
+            btnStop = new Button();
+            cmbWindowTitles = new ComboBox();
+            btnRefresh = new Button();
+            lblStatus = new Label();
+            lvSchedule = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            dateTimePicker1 = new DateTimePicker();
+            dtpScheduleTime = new DateTimePicker();
+            txtScheduleMessage = new TextBox();
+            btnAddSchedule = new Button();
+            btnDeleteSchedule = new Button();
+            label1 = new Label();
+            label3 = new Label();
+            lvAlarms = new ListView();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            label4 = new Label();
+            label5 = new Label();
+            txtAlarmMessage = new TextBox();
+            label6 = new Label();
+            dtpAlarmTime = new DateTimePicker();
+            btnAddAlarm = new Button();
+            btnDeleteAlarm = new Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(541, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Target Window Title";
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(374, 172);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 6;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(695, 172);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(75, 23);
+            btnStop.TabIndex = 7;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // cmbWindowTitles
+            // 
+            cmbWindowTitles.FormattingEnabled = true;
+            cmbWindowTitles.Location = new Point(504, 122);
+            cmbWindowTitles.Name = "cmbWindowTitles";
+            cmbWindowTitles.Size = new Size(181, 23);
+            cmbWindowTitles.TabIndex = 8;
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(695, 122);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(75, 23);
+            btnRefresh.TabIndex = 10;
+            btnRefresh.Text = "Refresh List";
+            btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(12, 64);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(39, 15);
+            lblStatus.TabIndex = 11;
+            lblStatus.Text = "Status";
+            // 
+            // lvSchedule
+            // 
+            lvSchedule.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3 });
+            lvSchedule.Location = new Point(374, 240);
+            lvSchedule.Name = "lvSchedule";
+            lvSchedule.Size = new Size(396, 198);
+            lvSchedule.TabIndex = 12;
+            lvSchedule.UseCompatibleStateImageBehavior = false;
+            lvSchedule.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Time";
+            columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Message";
+            columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Target Window";
+            columnHeader3.Width = 142;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(570, 27);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 13;
+            // 
+            // dtpScheduleTime
+            // 
+            dtpScheduleTime.Format = DateTimePickerFormat.Time;
+            dtpScheduleTime.Location = new Point(570, 56);
+            dtpScheduleTime.Name = "dtpScheduleTime";
+            dtpScheduleTime.Size = new Size(200, 23);
+            dtpScheduleTime.TabIndex = 14;
+            // 
+            // txtScheduleMessage
+            // 
+            txtScheduleMessage.Location = new Point(259, 30);
+            txtScheduleMessage.Name = "txtScheduleMessage";
+            txtScheduleMessage.Size = new Size(267, 23);
+            txtScheduleMessage.TabIndex = 15;
+            // 
+            // btnAddSchedule
+            // 
+            btnAddSchedule.Location = new Point(522, 172);
+            btnAddSchedule.Name = "btnAddSchedule";
+            btnAddSchedule.Size = new Size(132, 23);
+            btnAddSchedule.TabIndex = 16;
+            btnAddSchedule.Text = "Add to Schedule";
+            btnAddSchedule.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteSchedule
+            // 
+            btnDeleteSchedule.Location = new Point(526, 211);
+            btnDeleteSchedule.Name = "btnDeleteSchedule";
+            btnDeleteSchedule.Size = new Size(128, 23);
+            btnDeleteSchedule.TabIndex = 17;
+            btnDeleteSchedule.Text = "Delete Selected";
+            btnDeleteSchedule.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(338, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 15);
+            label1.TabIndex = 18;
+            label1.Text = "SET MESSAGE HERE";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(638, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 15);
+            label3.TabIndex = 19;
+            label3.Text = "DATE AND TIME";
+            label3.Click += label3_Click;
+            // 
+            // lvAlarms
+            // 
+            lvAlarms.Columns.AddRange(new ColumnHeader[] { columnHeader4, columnHeader5 });
+            lvAlarms.Location = new Point(12, 240);
+            lvAlarms.Name = "lvAlarms";
+            lvAlarms.Size = new Size(304, 198);
+            lvAlarms.TabIndex = 20;
+            lvAlarms.UseCompatibleStateImageBehavior = false;
+            lvAlarms.View = View.Details;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Reminder";
+            columnHeader4.Width = 150;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Countdown";
+            columnHeader5.Width = 150;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(91, 219);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 15);
+            label4.TabIndex = 21;
+            label4.Text = "Upcoming Alarms";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(13, 95);
+            label5.Name = "label5";
+            label5.Size = new Size(88, 15);
+            label5.TabIndex = 22;
+            label5.Text = "Alarm Message";
+            // 
+            // txtAlarmMessage
+            // 
+            txtAlarmMessage.Location = new Point(12, 113);
+            txtAlarmMessage.Name = "txtAlarmMessage";
+            txtAlarmMessage.Size = new Size(303, 23);
+            txtAlarmMessage.TabIndex = 23;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 139);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 15);
+            label6.TabIndex = 24;
+            label6.Text = "Alarm Time";
+            // 
+            // dtpAlarmTime
+            // 
+            dtpAlarmTime.CustomFormat = "MM/dd/yyyy hh:mm tt";
+            dtpAlarmTime.Format = DateTimePickerFormat.Custom;
+            dtpAlarmTime.Location = new Point(12, 157);
+            dtpAlarmTime.Name = "dtpAlarmTime";
+            dtpAlarmTime.Size = new Size(200, 23);
+            dtpAlarmTime.TabIndex = 25;
+            // 
+            // btnAddAlarm
+            // 
+            btnAddAlarm.Location = new Point(12, 186);
+            btnAddAlarm.Name = "btnAddAlarm";
+            btnAddAlarm.Size = new Size(75, 23);
+            btnAddAlarm.TabIndex = 26;
+            btnAddAlarm.Text = "Add Alarm";
+            btnAddAlarm.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAlarm
+            // 
+            btnDeleteAlarm.Location = new Point(102, 186);
+            btnDeleteAlarm.Name = "btnDeleteAlarm";
+            btnDeleteAlarm.Size = new Size(130, 23);
+            btnDeleteAlarm.TabIndex = 27;
+            btnDeleteAlarm.Text = "Delete Selected Alarm";
+            btnDeleteAlarm.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDeleteAlarm);
+            Controls.Add(btnAddAlarm);
+            Controls.Add(dtpAlarmTime);
+            Controls.Add(label6);
+            Controls.Add(txtAlarmMessage);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(lvAlarms);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(btnDeleteSchedule);
+            Controls.Add(btnAddSchedule);
+            Controls.Add(txtScheduleMessage);
+            Controls.Add(dtpScheduleTime);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(lvSchedule);
+            Controls.Add(lblStatus);
+            Controls.Add(btnRefresh);
+            Controls.Add(cmbWindowTitles);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private Label label2;
+        private Button btnStart;
+        private Button btnStop;
+        private ComboBox cmbWindowTitles;
+        private Button btnRefresh;
+        private Label lblStatus;
+        private ListView lvSchedule;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dtpScheduleTime;
+        private TextBox txtScheduleMessage;
+        private Button btnAddSchedule;
+        private Button btnDeleteSchedule;
+        private Label label1;
+        private Label label3;
+        private ListView lvAlarms;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private Label label4;
+        private Label label5;
+        private TextBox txtAlarmMessage;
+        private Label label6;
+        private DateTimePicker dtpAlarmTime;
+        private Button btnAddAlarm;
+        private Button btnDeleteAlarm;
+    }
+}
---
+++ LiveActivityApp/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LiveActivityApp
+{
+    public partial class Form1 : Form
+    {
+        // --- WinAPI & SendInput definitions ---
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        // --- Constants and Fields ---
+        private const int INPUT_KEYBOARD = 1;
+        private const uint KEYEVENTF_KEYUP = 0x0002;
+        private const uint KEYEVENTF_SCANCODE = 0x0008;
+        private const ushort SC_ENTER = 0x1C;
+
+        // The main timer now runs continuously to update alarms.
+        private readonly System.Windows.Forms.Timer _masterTimer = new System.Windows.Forms.Timer();
+        // This flag controls whether the scheduler part of the timer is active.
+        private bool _isSchedulerRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupApplication();
+        }
+
+        private void SetupApplication()
+        {
+            // The master timer starts immediately and runs forever.
+            _masterTimer.Interval = 1000;
+            _masterTimer.Tick += MasterTimer_Tick;
+            _masterTimer.Start();
+
+            // Wire up ALL button click events here for reliability
+            btnRefresh.Click += btnRefresh_Click;
+            btnAddSchedule.Click += btnAddSchedule_Click;
+            btnDeleteSchedule.Click += btnDeleteSchedule_Click;
+            btnStart.Click += btnStart_Click;
+            btnStop.Click += btnStop_Click;
+            btnAddAlarm.Click += btnAddAlarm_Click;
+            btnDeleteAlarm.Click += btnDeleteAlarm_Click;
+
+            // Initial UI state
+            this.Text = "Alarm and Auto Tool"; // SET INITIAL WINDOW TITLE
+            btnStop.Enabled = false;
+            lblStatus.Text = "Status: Idle.";
+            txtScheduleMessage.Text = "Type your message here";
+            txtAlarmMessage.Text = "Type Alarm message here";
+
+            PopulateRunningApps();
+        }
+
+        // --- Main Timer Tick (Handles BOTH systems) ---
+        private void MasterTimer_Tick(object? sender, EventArgs e)
+        {
+            // The alarm display updates every single second, unconditionally.
+            UpdateAlarmsDisplay();
+
+            // The scheduler logic ONLY runs if the user has clicked "Start".
+            if (_isSchedulerRunning)
+            {
+                RunSchedulerCheck();
+            }
+        }
+
+        // --- SCHEDULER Section ---
+
+        private void RunSchedulerCheck()
+        {
+            var now = DateTime.Now;
+            for (int i = lvSchedule.Items.Count - 1; i >= 0; i--)
+            {
+                if (lvSchedule.Items[i].Tag is ScheduleEntry entry)
+                {
+                    if (now >= entry.ScheduledTime && (now - entry.ScheduledTime) < TimeSpan.FromSeconds(2))
+                    {
+                        ExecuteScheduledAction(entry);
+                        lvSchedule.Items.RemoveAt(i);
+                    }
+                }
+            }
+
+            if (lvSchedule.Items.Count == 0)
+            {
+                // Auto-stop if the schedule becomes empty
+                btnStop_Click(this, EventArgs.Empty);
+                lblStatus.Text = "Status: All scheduled tasks complete. Scheduler stopped.";
+            }
+        }
+
+        private void btnAddSchedule_Click(object sender, EventArgs e)
+        {
+            if (cmbWindowTitles.SelectedIndex < 0 || string.IsNullOrWhiteSpace(txtScheduleMessage.Text))
+            {
+                lblStatus.Text = "Status: Select a window and enter a message for the schedule.";
+                return;
+            }
+
+            var selectedWindow = (WindowInfo)cmbWindowTitles.Items[cmbWindowTitles.SelectedIndex];
+            var newEntry = new ScheduleEntry(dtpScheduleTime.Value, txtScheduleMessage.Text, selectedWindow);
+            var listViewItem = new ListViewItem(newEntry.ScheduledTime.ToString("HH:mm:ss"));
+            listViewItem.SubItems.Add(newEntry.Message);
+            listViewItem.SubItems.Add(newEntry.TargetWindow.Title);
+            listViewItem.Tag = newEntry;
+            lvSchedule.Items.Add(listViewItem);
+            lblStatus.Text = $"Status: Added schedule for {newEntry.ScheduledTime:g}."; // Using "g" for short date/time
+        }
+
+        private void ExecuteScheduledAction(ScheduleEntry entry)
+        {
+            if (!IsWindow(entry.TargetWindow.Handle))
+            {
+                lblStatus.Text = $"Status: SKIPPED. Window '{entry.TargetWindow.Title}' was closed.";
+                return;
+            }
+            SetForegroundWindow(entry.TargetWindow.Handle);
+            Thread.Sleep(200);
+            SendKeys.SendWait(entry.Message + "{ENTER}");
+            lblStatus.Text = $"Status: EXECUTED '{entry.Message}' at {DateTime.Now:T}.";
+        }
+
+        // --- ALARM Section ---
+
+        private void UpdateAlarmsDisplay()
+        {
+            // Iterates through the alarm list and updates countdowns in place.
+            foreach (ListViewItem item in lvAlarms.Items)
+            {
+                if (item.Tag is AlarmEntry entry)
+                {
+                    TimeSpan timeRemaining = entry.AlarmTime - DateTime.Now;
+                    if (timeRemaining.TotalSeconds > 0)
+                    {
+                        // Now handles days correctly
+                        item.SubItems[1].Text = timeRemaining.ToString(@"d\.hh\:mm\:ss");
+                        item.BackColor = timeRemaining.TotalSeconds < 10 ? Color.LightCoral : SystemColors.Window;
+                    }
+                    else
+                    {
+                        item.SubItems[1].Text = "Finished";
+                        item.BackColor = Color.LightGray;
+                    }
+                }
+            }
+        }
+
+        private void btnAddAlarm_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAlarmMessage.Text))
+            {
+                lblStatus.Text = "Status: Please enter a message for the alarm.";
+                return;
+            }
+            var newEntry = new AlarmEntry(dtpAlarmTime.Value, txtAlarmMessage.Text);
+            var alarmItem = new ListViewItem(newEntry.Message);
+            alarmItem.SubItems.Add("Calculating..."); // Placeholder
+            alarmItem.Tag = newEntry;
+            lvAlarms.Items.Add(alarmItem);
+            // Updated status message to show the full date and time for clarity
+            lblStatus.Text = $"Status: Added alarm for {newEntry.AlarmTime:g}.";
+        }
+
+        private void btnDeleteAlarm_Click(object sender, EventArgs e)
+        {
+            if (lvAlarms.SelectedItems.Count == 0)
+            {
+                lblStatus.Text = "Status: Select an alarm from the list to delete.";
+                return;
+            }
+            foreach (ListViewItem selectedItem in lvAlarms.SelectedItems)
+                lvAlarms.Items.Remove(selectedItem);
+            lblStatus.Text = "Status: Selected alarm(s) deleted.";
+        }
+
+
+        // --- General UI and Helper Methods ---
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            PopulateRunningApps();
+            lblStatus.Text = "Status: Window list has been refreshed.";
+        }
+
+        private void btnDeleteSchedule_Click(object sender, EventArgs e)
+        {
+            if (lvSchedule.SelectedItems.Count == 0) return;
+            foreach (ListViewItem selectedItem in lvSchedule.SelectedItems)
+                lvSchedule.Items.Remove(selectedItem);
+            lblStatus.Text = "Status: Selected schedule(s) deleted.";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (lvSchedule.Items.Count == 0)
+            {
+                lblStatus.Text = "Status: Cannot start. The schedule is empty.";
+                return;
+            }
+            _isSchedulerRunning = true; // Set the flag
+            LockSchedulerUI(true);
+            Text = "Alarm and Auto Tool - RUNNING"; // UPDATE WINDOW TITLE
+            lblStatus.Text = "Status: Scheduler is running...";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _isSchedulerRunning = false; // Clear the flag
+            LockSchedulerUI(false);
+            Text = "Alarm and Auto Tool - STOPPED"; // UPDATE WINDOW TITLE
+            lblStatus.Text = "Status: Scheduler stopped.";
+        }
+
+        private void PopulateRunningApps()
+        {
+            cmbWindowTitles.Items.Clear();
+            try
+            {
+                foreach (Process p in Process.GetProcesses())
+                {
+                    if (p.MainWindowHandle != IntPtr.Zero && !string.IsNullOrEmpty(p.MainWindowTitle))
+                    {
+                        cmbWindowTitles.Items.Add(new WindowInfo { Title = p.MainWindowTitle, Handle = p.MainWindowHandle });
+                    }
+                }
+            }
+            catch { /* Ignore errors */ }
+            if (cmbWindowTitles.Items.Count > 0) cmbWindowTitles.SelectedIndex = 0;
+        }
+
+        // This now ONLY locks the controls related to the scheduler.
+        private void LockSchedulerUI(bool isRunning)
+        {
+            btnStart.Enabled = !isRunning;
+            btnStop.Enabled = isRunning;
+            // The following controls are part of the scheduler function
+            btnRefresh.Enabled = !isRunning;
+            cmbWindowTitles.Enabled = !isRunning;
+            dtpScheduleTime.Enabled = !isRunning;
+            txtScheduleMessage.Enabled = !isRunning;
+            btnAddSchedule.Enabled = !isRunning;
+            btnDeleteSchedule.Enabled = !isRunning;
+        }
+
+        // --- P/Invoke structs and Keystroke helpers ---
+        private static void SendKeyScan(ushort scanCode, bool keyUp)
+        {
+            var input = new INPUT[1];
+            input[0].type = INPUT_KEYBOARD;
+            input[0].U.ki.wScan = scanCode;
+            input[0].U.ki.dwFlags = KEYEVENTF_SCANCODE | (keyUp ? KEYEVENTF_KEYUP : 0);
+            SendInput(1, input, Marshal.SizeOf(typeof(INPUT)));
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct INPUT { public int type; public InputUnion U; }
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct InputUnion { [FieldOffset(0)] public KEYBDINPUT ki; }
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct KEYBDINPUT { public ushort wVk; public ushort wScan; public uint dwFlags; public uint time; public IntPtr dwExtraInfo; }
+    }
+
+    // --- Data Classes ---
+    public class ScheduleEntry
+    {
+        public DateTime ScheduledTime { get; set; }
+        public string Message { get; set; }
+        public WindowInfo TargetWindow { get; set; }
+        public ScheduleEntry(DateTime time, string message, WindowInfo target)
+        {
+            ScheduledTime = time; Message = message; TargetWindow = target;
+        }
+    }
+
+    public class AlarmEntry
+    {
+        public DateTime AlarmTime { get; set; }
+        public string Message { get; set; }
+        public AlarmEntry(DateTime time, string message)
+        {
+            AlarmTime = time; Message = message;
+        }
+    }
+
+    public class WindowInfo
+    {
+        public string Title { get; set; } = "";
+        public IntPtr Handle { get; set; }
+        public override string ToString() => Title;
+    }
+}
+
